302f23fb95edd240e44d40da58025d7cad9e8dfd
Parham Alvani <parham.alvani@gmail.com>
2022-02-07 23:26:45 +0000

    feat: add configuration, retries and csv results

--- cmd/s1-gw-ns/main.go
+++ cmd/s1-gw-ns/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"time"
+	"encoding/csv"
+	"os"
 
 	"github.com/citado/s1-gw-ns/internal/app"
-	"github.com/citado/s1-gw-ns/internal/lora"
+	"github.com/citado/s1-gw-ns/internal/config"
 	"github.com/pterm/pterm"
 )
 
@@ -15,27 +16,38 @@ func main() {
 		pterm.NewLettersFromStringWithStyle("-1", pterm.NewStyle(pterm.FgLightGreen))).
 		Render()
 
-	a := app.New(app.Config{
-		Addr:  "127.0.0.1",
-		Port:  1883,
-		Total: 10,
-		Delay: 100 * time.Millisecond,
-	})
+	cfg := config.New()
 
-	a.Gateway(lora.Config{
-		MAC: "b827ebffff70c80a",
-		Keys: lora.Keys{
-			NetworkSKey:     "DB56B6C3002A4763A79E64573C629D97",
-			ApplicationSKey: "94B49CD7BC621BC46571D019640804AA",
-		},
-		Device: lora.Device{
-			Addr: "26011CF6",
-		},
-	})
+	a := app.New(cfg.App)
+
+	f, err := os.Create("result.csv")
+	if err != nil {
+		pterm.Fatal.Printf("cannot create result.cvs %s", err)
+	}
+	defer f.Close()
+
+	w := csv.NewWriter(f)
+
+	for _, g := range cfg.Gateways {
+		a.Gateway(g)
+	}
 
 	a.Connect()
 
-	a.Run()
+	for i := 0; i < cfg.Tries; i++ {
+		a.Run()
 
-	pterm.Success.Println(a.Durations)
+		pterm.Success.Println(a.Durations)
+
+		r := make([]string, 0)
+		for _, d := range a.Durations {
+			r = append(r, d.String())
+		}
+
+		if err := w.Write(r); err != nil {
+			pterm.Fatal.Printf("cannot write to result.cvs %s", err)
+		}
+
+		w.Flush()
+	}
 }

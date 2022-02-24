7f34bac14a785242b6cc6f3f0677ce84f95889fc
Parham Alvani <parham.alvani@gmail.com>
2022-02-24 17:44:41 +0000

    feat: add gateway api

--- internal/lora/api/gateway.go
+++ internal/lora/api/gateway.go
@@ -1,1 +1,16 @@
 package api
+
+type Gateway struct {
+	ID               string `json:"id"`
+	Name             string `json:"name"`
+	Description      string `json:"description"`
+	OrganizationID   string `json:"organizationID"` // nolint: tagliatelle
+	DiscoveryEnabled bool   `json:"discoveryEnabled"`
+	NetworkServerID  string `json:"networServerID"`   // nolint: tagliatelle
+	GatewayProfileID string `json:"gatewayProfileID"` // nolint: tagliatelle
+	ServiceProfileID string `json:"serviceProfileID"` // nolint: tagliatelle
+}
+
+func (a API) CreateGateway() error {
+	return nil
+}

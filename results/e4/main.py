02c042b3553399a259ae3c78ef8756e4583f5c29
Parham Alvani <parham.alvani@gmail.com>
2022-02-25 08:10:39 +0330

    feat: add mean latency plot

--- results/e4/main.py
+++ results/e4/main.py
@@ -16,9 +16,11 @@ names: List[str] = ["device_id"] + [str(i) for i in range(0, 1000)]
 # delivery_ratio for each examined rate.
 # for each rate we have [min, mean, max]
 delivery_ratio: Dict[str, np.ndarray] = {}
+mean_latency: Dict[str, np.ndarray] = {}
 
 for rate in ["s1", "ms500", "ms100"]:
     delivery_ratio_per_try: np.ndarray = np.zeros(10)
+    mean_latency_per_try: np.ndarray = np.zeros(10)
 
     for t in range(0, 10):
         print(f"reading {rate} {t+1}")
@@ -54,12 +56,21 @@ for rate in ["s1", "ms500", "ms100"]:
         print(f"\n{d.describe()}")
 
         delivery_ratio_per_try[t] = delivery_ratio_per_device.mean()
+        mean_latency_per_try[t] = mean_latency_per_device.mean()
 
     delivery_ratio[rate] = np.array(
         [
             delivery_ratio_per_try.min(),
             delivery_ratio_per_try.mean(),
             delivery_ratio_per_try.max(),
+        ]
+    )
+
+    mean_latency[rate] = np.array(
+        [
+            mean_latency_per_try.min(),
+            mean_latency_per_try.mean(),
+            mean_latency_per_try.max(),
         ]
     )
 
@@ -78,3 +89,17 @@ ax.errorbar(
 ax.set_title("Packet Delivery Ratio")
 ax.set(ylabel="Delivery Ratio (%)", xlabel="Packet Rate (pps)")
 fig.savefig("drop.png")
+
+fig, ax = plt.subplots(figsize=(10, 10))
+ax.errorbar(
+    x=[k for k in mean_latency.keys()],
+    y=[v[1] for v in mean_latency.values()],
+    fmt="r--",
+    yerr=[
+        [v[1] - v[0] for v in mean_latency.values()],
+        [v[2] - v[1] for v in mean_latency.values()],
+    ],
+)
+ax.set_title("Latency")
+ax.set(ylabel="Average Delay (s)", xlabel="Packet Rate (pps)")
+fig.savefig("latency.png")

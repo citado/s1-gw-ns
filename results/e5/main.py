7c3d930736f19a45f0c3192ca20c6dd2a5a09669
Parham Alvani <parham.alvani@gmail.com>
2022-02-26 09:37:30 +0330

    feat: add reporter for e5

---
+++ results/e5/main.py
@@ -0,0 +1,135 @@
+"""
+this modules load and process the forth experiment of
+the chirpstack load testing scenario.
+"""
+from typing import Dict, List
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+TOTAL_MESSAGES = 1000
+
+# name indicates column names
+names: List[str] = ["device_id"] + [str(i) for i in range(0, 1000)]
+
+# delivery_ratio for each examined rate.
+# for each number of nodes we have [min, mean, max]
+delivery_ratio: Dict[str, np.ndarray] = {}
+mean_latency: Dict[str, np.ndarray] = {}
+p90_latency: Dict[str, np.ndarray] = {}
+
+for rate in ["n1", "n10"]:
+    delivery_ratio_per_try: np.ndarray = np.zeros(10)
+    mean_latency_per_try: np.ndarray = np.zeros(10)
+    p90_latency_per_try: np.ndarray = np.zeros(10)
+
+    for t in range(0, 10):
+        print(f"reading {rate} {t+1}")
+        d = pd.DataFrame(
+            pd.read_csv(
+                f"{rate}_{t+1}.csv",
+                header=None,
+                names=names,
+                engine="python",
+                on_bad_lines=lambda bad_line: bad_line[:TOTAL_MESSAGES]
+                if len(bad_line) > TOTAL_MESSAGES
+                else None,
+            )
+        )
+
+        # drop device_id if it exists
+        if (x := d.drop("device_id", axis=1)) is not None:
+            d = x
+
+        delivery_ratio_per_device: np.ndarray = (
+            d.notna().sum(axis=1) / TOTAL_MESSAGES * 100
+        ).to_numpy()
+        print("delivery ratio:")
+        print(f"\t mean: {delivery_ratio_per_device.mean()}%")
+        print(f"\t min: {delivery_ratio_per_device.min()}%")
+        print(f"\t max: {delivery_ratio_per_device.max()}%")
+
+        mean_latency_per_device: np.ndarray = np.nanmean(d.to_numpy(), axis=1)
+        max_latency_per_device: np.ndarray = np.nanmax(d.to_numpy(), axis=1)
+        min_latency_per_device: np.ndarray = np.nanmin(d.to_numpy(), axis=1)
+        p90_latency_per_device: np.ndarray = np.nanquantile(
+            d.to_numpy(), axis=1, q=0.90
+        )
+
+        print("latency:")
+        print(f"\t mean: {mean_latency_per_device.mean()}")
+        print(f"\t min: {min_latency_per_device.min()}")
+        print(f"\t max: {max_latency_per_device.max()}")
+
+        print(f"\n{d.describe()}")
+
+        delivery_ratio_per_try[t] = delivery_ratio_per_device.mean()
+        mean_latency_per_try[t] = mean_latency_per_device.mean()
+        p90_latency_per_try[t] = p90_latency_per_device.mean()
+
+    delivery_ratio[rate] = np.array(
+        [
+            delivery_ratio_per_try.min(),
+            delivery_ratio_per_try.mean(),
+            delivery_ratio_per_try.max(),
+        ]
+    )
+
+    mean_latency[rate] = np.array(
+        [
+            mean_latency_per_try.min(),
+            mean_latency_per_try.mean(),
+            mean_latency_per_try.max(),
+        ]
+    )
+
+    p90_latency[rate] = np.array(
+        [
+            p90_latency_per_try.min(),
+            p90_latency_per_try.mean(),
+            p90_latency_per_try.max(),
+        ]
+    )
+
+fig, ax = plt.subplots(figsize=(10, 10))
+ax.errorbar(
+    x=list(delivery_ratio.keys()),
+    y=[v[1] for v in delivery_ratio.values()],
+    fmt="g--",
+    yerr=[
+        [v[1] - v[0] for v in delivery_ratio.values()],
+        [v[2] - v[1] for v in delivery_ratio.values()],
+    ],
+)
+ax.set_title("Packet Delivery Ratio")
+ax.set(ylabel="Delivery Ratio (%)", xlabel="Number of Devices")
+fig.savefig("drop.png")
+
+fig, ax = plt.subplots(figsize=(10, 10))
+ax.errorbar(
+    x=list(mean_latency.keys()),
+    y=[v[1] for v in mean_latency.values()],
+    fmt="r--",
+    yerr=[
+        [v[1] - v[0] for v in mean_latency.values()],
+        [v[2] - v[1] for v in mean_latency.values()],
+    ],
+)
+ax.set_title("Latency")
+ax.set(ylabel="Average Delay (s)", xlabel="Number of Devices")
+fig.savefig("latency.png")
+
+fig, ax = plt.subplots(figsize=(10, 10))
+ax.errorbar(
+    x=list(p90_latency.keys()),
+    y=[v[1] for v in p90_latency.values()],
+    fmt="b--",
+    yerr=[
+        [v[1] - v[0] for v in p90_latency.values()],
+        [v[2] - v[1] for v in p90_latency.values()],
+    ],
+)
+ax.set_title("P90 Latency")
+ax.set(ylabel="P90 Delay (s)", xlabel="Number of Devices")
+fig.savefig("p90_latency.png")
